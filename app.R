f1d107c12b3180c9e33e6984a7a5161a848265f8
lulunac27a <100660343+lulunac27a@users.noreply.github.com>
2024-12-26 12:38:11 -0600

    Format R code

--- app.R
+++ app.R
@@ -1,35 +1,26 @@
 library(shiny)
 library(stringr)
 
-ui <- fluidPage(
-    titlePanel("Text Counter Application in R Shiny"),
-    sidebarLayout(
-        sidebarPanel(
-            textAreaInput("text", "Enter text:", rows = 10, cols = 80),
-            fileInput("file", "Upload a .txt file", accept = ".txt")
-        ),
-        mainPanel(
-            textOutput("textOutput")
-        )
-    )
-)
+ui <- fluidPage(titlePanel("Text Counter Application in R Shiny"),
+  sidebarLayout(sidebarPanel(textAreaInput("text", "Enter text:",
+    rows = 10, cols = 80), fileInput("file", "Upload a .txt file",
+    accept = ".txt")), mainPanel(textOutput("textOutput"))))
 
 server <- function(input, output) {
-    output$textOutput <- renderText({
-        text <- input$text
-        
-        if (!is.null(input$file)) {
-            text <- readLines(input$file$datapath)
-            text <- paste(text, collapse = "\n")
-        }
-        
-        chars <- nchar(text)
-        words <- str_count(text, "\\S+")
-        lines <- str_count(text, "\n") + 1
-        paste("Characters:", chars,
-              "\nWords:", words,
-              "\nLines:", lines)
-    })
+  output$textOutput <- renderText({
+    text <- input$text
+
+    if (!is.null(input$file)) {
+      text <- readLines(input$file$datapath)
+      text <- paste(text, collapse = "\n")
+    }
+
+    chars <- nchar(text)
+    words <- str_count(text, "\\S+")
+    lines <- str_count(text, "\n") + 1
+    paste("Characters:", chars, "\nWords:", words, "\nLines:",
+      lines)
+  })
 }
 
 shinyApp(ui = ui, server = server)
